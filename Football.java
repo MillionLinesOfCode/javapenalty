ab15a0f8f9d474723e058de6a5faf0f71e360de8
reeyanmaknojiya <47335900+reeyanmaknojiya@users.noreply.github.com>
2019-02-24 16:43:12 -0700

    Updated all the methods
    Updated the methods and changed the default values to correct return values using if-else statements.

--- Football.java
+++ Football.java
@@ -19,33 +19,31 @@ public class Football {
 	// Methods
 	// This method checks if the ball hit the goalpost or not
 	public boolean hitsGoalpost(Goalpost goalpost){
-            //if (location == Goalpost.getLocation()) {
+            if (location == goalpost.getLocation()) {
 		return true;
             }    
-            //else if (location != Goalpost.getLocation()) {
-                //return false;
-            //}
-	//}
-	// This method checks if the goal was saved by the goal keeper
+            else if (location != goalpost.getLocation());
+                return false;
+	}
+	//This method checks if the goal was saved by the goal keeper
 	public boolean SavedByGoalkeeper(Goalkeeper goalkeeper){
-            //if (location == Goalkeeper.getLocation()) {
+            if (location == goalkeeper.getLocation()) {
 		return true;
-            //}    
-            //else if (location != Goalpost.getLocation()) {
-                //return false;
-            //}
+            }    
+            else if (location != goalkeeper.getLocation());
+                return false;
 	}
         // This method checks if the shot was on target
 	public boolean onTarget(Goal goal) {
-		return true;
+            return (location == goal.getLocation()); 
 	}
         // This method checks if the shot was off target
 	public boolean offTarget(Goal goal) {
-		return false;
+            return (location != goal.getLocation());
 	}
         // This method will return the location of the football
         public Rectangle getLocation() {
-		return location;
+            return location;
 	}
 
 }
