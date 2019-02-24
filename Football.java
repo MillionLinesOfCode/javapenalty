806845407ea494807fa96896fe32093fccfaa351
reeyanmaknojiya <47335900+reeyanmaknojiya@users.noreply.github.com>
2019-02-24 16:46:55 -0700

    Updated methods and made them concise

--- Football.java
+++ Football.java
@@ -2,8 +2,8 @@
 import java.awt.Rectangle;
 
 /**
-	This is a class for the ball
-	It has methods that check the outcome of the shoot
+	This is a class for the football
+	It has methods that check the outcome of the shot
 */
 
 public class Football {
@@ -15,23 +15,14 @@ public class Football {
 	public Football(Rectangle location){
 		location = new Rectangle(0, 0, 0, 0);
 	}
-
 	// Methods
 	// This method checks if the ball hit the goalpost or not
 	public boolean hitsGoalpost(Goalpost goalpost){
-            if (location == goalpost.getLocation()) {
-		return true;
-            }    
-            else if (location != goalpost.getLocation());
-                return false;
-	}
+            return (location == goalpost.getLocation());
+        }
 	//This method checks if the goal was saved by the goal keeper
 	public boolean SavedByGoalkeeper(Goalkeeper goalkeeper){
-            if (location == goalkeeper.getLocation()) {
-		return true;
-            }    
-            else if (location != goalkeeper.getLocation());
-                return false;
+            return (location == goalkeeper.getLocation());
 	}
         // This method checks if the shot was on target
 	public boolean onTarget(Goal goal) {
