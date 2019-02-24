d2add29120071f6b12ebb03efdc1e3c5ec1eebd9
reeyanmaknojiya <47335900+reeyanmaknojiya@users.noreply.github.com>
2019-02-24 16:54:41 -0700

    Updated methods
    Updated the methods and changed from default values to proper return values using conditions

--- Goal.java
+++ Goal.java
@@ -6,13 +6,11 @@ public class Goal{
     /*
      *  The net of the goalpost.
      *  This method essentially compares the location of the ball which is shot by the player to the location of the net.
-     *  
-     * 
-     */
+    */
     //Methods
     //This method returns true if the player scores a goal i.e. if the location of the goal and the ball is same.
-    public Boolean hasTheBall(){
-        return true;
+    public Boolean hasTheBall(Football football){
+        return (football.getLocation() == location);
     }
     //This method returns the location of the goal
     public Rectangle getLocation(){
