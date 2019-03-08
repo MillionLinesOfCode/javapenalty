abe7ebaa9d8a52a2db225366cd41d835d5839ed2
shehzan31 <47336141+shehzan31@users.noreply.github.com>
2019-03-08 13:25:19 -0700

    Add files via upload

---
+++ AnimationApp.java
@@ -0,0 +1,211 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.text.Font;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.geometry.Pos;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.Group;
+import javafx.scene.text.Font;
+
+/*
+This is the AnimationApp class, which controls the entire application. 
+1. Interact with the human player to ask it for a move.
+2. Move the avatar as specified
+3. Check if any obstacles are hit.  If so react appropriately.
+4. Check if any collectibles have been reached.  If so, react appropriately.
+5. Move any collectibles that are movable.
+
+*/
+public class AnimationApp extends Application{
+	// These are the private instance variables:
+	private Avatar avatar = new Avatar();
+	private Goalkeeper goalkeeper = new Goalkeeper();
+	private ArrayList<Football> football = new ArrayList<Football>();
+	private ArrayList<Goal> goal = new ArrayList<Goal>();
+	private ArrayList<Goalpost> goalpost = new ArrayList<Goalpost>();
+        
+	// Methods
+	public void displayAvatarShot() {
+		//GUI
+	// It asks the human for input and then shows the animation as per input
+	}
+	
+	public void displayGoalkeeperSave() {
+		//GUI
+	// Based on the Goalkeeper's random decision to dive, it will dive at one side regardless of the player shooting the ball.
+	// If the keeper dives at the same side as the ball was kicked, it won't let the ball get inside the post.
+	}
+	
+	public int printCurrentScore() {
+	// From the arraylist maintained for score it will take the number of goals and print the result .
+		return goal.size();
+	}
+	
+	public void drawCurrentScore() {
+		//GUI
+	// Draws the score on the screen, from the above function "printCurrentScore()"
+	}
+	
+	public void performAvatarCelebration() {
+		//GUI
+	// Depending on the output of the situation, avatar will accordingly perform a celebration.
+	}
+	
+	public void performGoalkeeperCelebration() {
+		//GUI
+	// Depending on the output of the situation, Goalkeeper will accordingly perform a celebration.
+	}
+/*
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Welcome to the Penalty Shooutout Game!");
+            System.out.println("Enter your name: ");
+            String userInput = sc.nextLine();
+            System.out.println("Hello" + ", " + userInput + "!");
+            System.out.println("Choose your favorite player to take the penalties");
+            System.out.println("1- C.Ronaldo");
+            System.out.println("2- L.Messi");
+            System.out.println("3- P.Pogba");
+            System.out.println("4- Neymar");
+            System.out.println("5- E.Hazard");
+            int playerOption = sc.nextInt();
+            switch (playerOption) {
+                case 1:
+                    Avatar ronaldo = new Avatar("C.Ronaldo", "Portugal");
+                    System.out.println("Great! You chose " + ronaldo.getName());
+                    break;
+                case 2:
+		    Avatar messi = new Avatar("L.Messi", "Argentina");
+                    System.out.println("Great! You chose " + messi.getName());
+                    break;
+                case 3:
+		    Avatar pogba = new Avatar("P.Pogba", "France");
+		    System.out.println("Great! You chose " + pogba.getName());
+                    break;
+                case 4:
+		    Avatar neymar = new Avatar("Neymar", "Brazil");
+		    System.out.println("Great! You chose " + neymar.getName());
+                    break;
+                case 5:
+		    Avatar hazard = new Avatar("E.Hazard", "Belgium");
+		    System.out.println("Great! You chose " + hazard.getName());
+                    break;
+                default:
+		    Avatar defaultPlayer = new Avatar("C.Ronaldo", "Portugal");
+		    System.out.println("Your default player is " + defaultPlayer.getName());
+                    break;
+            }
+            System.out.println("Choose your favorite goalkeeper to save the penalties");
+            System.out.println("1- D.de Gea");
+            System.out.println("2- J.Oblak");
+            System.out.println("3- M.ter Stegen");
+            System.out.println("4- T.Courtois");
+            System.out.println("5- M.Neuer");
+            int goalkeeperOption = sc.nextInt();
+            switch (goalkeeperOption) {
+                case 1:
+		    Goalkeeper gea = new Goalkeeper("D.de Gea", "Spain");
+                    System.out.println("Cool! You chose " + gea.getName());
+                    break;
+                case 2:
+		    Goalkeeper oblak = new Goalkeeper("J.Oblak", "Slovenia");
+                    System.out.println("Cool! You chose " + oblak.getName());
+                    break;
+                case 3:
+		    Goalkeeper stegen = new Goalkeeper("M.ter Stegen", "Germany");
+                    System.out.println("Cool! You chose " + stegen.getName());
+                    break;
+                case 4:
+		    Goalkeeper courtois = new Goalkeeper("T.Courtois", "Belgium");
+                    System.out.println("Cool! You chose " + courtois.getName());
+                    break;
+                case 5:
+		    Goalkeeper neuer = new Goalkeeper("M.Neuer", "Germany");
+                    System.out.println("Cool! You chose " + neuer.getName());
+                    break;
+                default:
+		    Goalkeeper defaultKeeper = new Goalkeeper("D.de Gea", "Spain");
+                    System.out.println("Your default keeper is " + defaultKeeper.getName());
+                    break;
+            }
+            //Ask the user for input in order to take the shot
+            System.out.println("Choose a number to take a shot \n" +
+                               " ____________________________  \n" +
+                               "|         |        |         | \n" +
+                               "|    1    |   2    |    3    | \n" +
+                               "|_________|________|_________| \n" +
+                               "|         |        |         | \n" +
+                               "|    4    |   5    |    6    | \n" +
+                               "|_________|________|_________| \n");
+            
+            for (int i = 0; i < 5; i++) {
+                int shotInput = sc.nextInt();
+                while (shotInput < 1 || shotInput > 6){
+                    System.out.println("Error! Please enter a valid number");
+                    shotInput = sc.nextInt();
+                }
+                Goalkeeper goalkeeper = new Goalkeeper("D.de Gea", "Spain");
+                goalkeeper.processGoalkeeperSave(shotInput, goalkeeper);
+                
+            }
+            System.out.println("Game over!");
+        }
+*/
+	public static void main(String[] args)
+   {
+      launch(args);
+   }
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Image bgImg = new Image("file:Background.jpg");
+		ImageView mv = new ImageView(bgImg);
+		mv.setFitWidth(700);
+		mv.setFitHeight(500);
+		Group bg = new Group();
+		bg.getChildren().addAll(mv);
+		BorderPane root = new BorderPane();
+		HBox topLabel = new HBox();
+		HBox centreLabel = new HBox();
+		Label label1 = new Label("Welcome to the Penalty Shooutout Game!");
+		Label label2 = new Label("Enter your Name: ");
+		label1.setFont(Font.font("Times New Roman", 24));
+		label1.setStyle("-fx-font-weight: bold");
+		//label1.setFill(Color.WHITE);
+		label2.setFont(Font.font("Castellar", 24));
+		label2.setStyle("-fx-font-weight: bold");
+		TextField name = new TextField("name");
+		topLabel.getChildren().add(label1);
+		centreLabel.getChildren().add(label2);
+		centreLabel.getChildren().add(name);
+		Button doneButton = new Button("Done");
+		centreLabel.getChildren().add(doneButton);
+		
+		topLabel.setAlignment(Pos.CENTER);
+		centreLabel.setAlignment(Pos.CENTER);
+		root.getChildren().add(bg);
+		
+		root.setTop(topLabel);
+		root.setCenter(centreLabel);
+		
+		
+		Scene scene = new Scene(root, 700, 500);
+		primaryStage.setTitle("Penalty Shootout Game");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+		
+ }
+
