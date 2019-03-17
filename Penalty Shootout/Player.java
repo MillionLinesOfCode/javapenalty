d1357ae773b029b76060ee66d8b69379f74f0ca3
takedii <47336291+takedii@users.noreply.github.com>
2019-03-17 14:44:58 -0600

    Organized the code from demo 2
    Added method for each scenes and created a function that is accountable for logic of the game.

---
+++ Penalty Shootout/Player.java
@@ -0,0 +1,50 @@
+import java.util.Random;
+
+public class Player {
+	// Instance Variables that represent the characteristics of the player that the user controls
+	private String name; 
+	private String nationality;
+	private int playerScore = 0;
+
+	// Constructors
+	public Player() {
+		this.name = "C.Ronaldo"; 
+		this.nationality = "Portugal";
+		this.playerScore = 0;
+	}
+	public Player(String name, String nationality) {
+		this.name = name;
+		this.nationality = nationality;
+		this.playerScore = 0;
+	}
+	// Getters and Setters
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNationality() {
+		return nationality;
+	}
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+
+	public int getPlayerScore() {
+		return playerScore;
+	}
+	public void setPlayerScore(int playerScore) {
+		this.playerScore += playerScore;
+	}
+
+	// Methods
+	//Generates a random number between 1 and 100(to be changed later)
+	public int randStrength() {
+		Random rand = new Random();
+		int n = rand.nextInt(100);
+		n += 1;
+		return n;
+	}
+}
