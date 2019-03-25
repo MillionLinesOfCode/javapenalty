5c2bb0078bed87d9681a061eb457eee444c68341
reeyanmaknojiya <47335900+reeyanmaknojiya@users.noreply.github.com>
2019-03-24 19:05:00 -0600

    Added a new method for editing labels

--- Penalty Shootout/DrawObjects.java
+++ Penalty Shootout/DrawObjects.java
@@ -1,5 +1,8 @@
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Font;
+
 
 public class DrawObjects {
 	public ImageView drawPlayer(String namePhoto, int setfh, int setfw, int x, int y) {
@@ -22,4 +25,10 @@ public class DrawObjects {
 		football.setOpacity(opac);
 		return football;
 	}
+        public Label editLabel(Label scoreUpdates, int x, int y, int setFontSize, String setFontName) {
+            scoreUpdates.setLayoutX(x);
+            scoreUpdates.setLayoutY(y);
+            scoreUpdates.setFont(Font.font(setFontName, setFontSize));
+            return scoreUpdates;
+        }
 }
