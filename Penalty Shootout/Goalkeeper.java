f8c21dbc213e9ac02abcd0b3b82fecd4f780a527
reeyanmaknojiya <47335900+reeyanmaknojiya@users.noreply.github.com>
2019-03-24 19:11:15 -0600

    Minor changes by RC

--- Penalty Shootout/Goalkeeper.java
+++ Penalty Shootout/Goalkeeper.java
@@ -1,13 +1,13 @@
 import java.util.Random;
-
 public class Goalkeeper {
-	// Instance Variables
+	//Instance Variables that represent the characteristics of the goalkeeper that is played by the user's system and is not playable by user.
 	private String name;
 	private String nationality;
-	private int goalkeeperScore = 0;
+	private int goalkeeperScore = 0; //Default value
 
 	// Constructors
 	public Goalkeeper() {
+		//Default values
 		this.name = "D.de Gea";
 		this.nationality = "Spain";
 		this.goalkeeperScore = 0;
@@ -38,11 +38,17 @@ public class Goalkeeper {
 		return goalkeeperScore;
 	}
 	public void setGoalkeeperScore(int goalkeeperScore) {
-		this.goalkeeperScore += goalkeeperScore;
+		if (goalkeeperScore > 0) {
+                    this.goalkeeperScore += goalkeeperScore;
+		}
 	}
 
-	// Methods
+	// Other Method(s)
 	public int randGoalkeeperNum() {
+		/*
+		Generates a random number between 1 and 6,
+		which the goalkeeper 'A.I.' uses to guess the direction in which the player shoots
+		*/
 		Random rand = new Random();
 		int n = rand.nextInt(6);
 		n += 1;
