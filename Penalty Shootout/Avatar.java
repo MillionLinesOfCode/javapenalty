acb97d1abe0e8326b523a62b1a79ae29e02fe8db
reeyanmaknojiya <47335900+reeyanmaknojiya@users.noreply.github.com>
2019-04-09 22:29:58 -0600

    Added a new abstract class
    This is an abstract class which has some defined some methods shared by the Player and Goalkeeper class. They will extend this class.

---
+++ Penalty Shootout/Avatar.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author Reeyan.mak
+ */
+
+//This is an abstract class which has some defined some methods shared by the Player and Goalkeeper class.
+public abstract class Avatar {
+    
+        public abstract String getName();
+    
+        public abstract void setName(String name);
+
+	public abstract String getNationality();
+        
+	public abstract void setNationality(String nationality);
+}
