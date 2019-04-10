17127cea1da837a13cee479a2ea04394eb277274
reeyanmaknojiya <47335900+reeyanmaknojiya@users.noreply.github.com>
2019-04-09 22:31:43 -0600

    Made changes so that it extends Avatar class

--- Penalty Shootout/Player.java
+++ Penalty Shootout/Player.java
@@ -1,43 +1,45 @@
-import java.util.*;
 
-public class Player {
+public class Player extends Avatar {
 	// Instance Variables that represent the characteristics of the player that the user controls
 	private String name; 
 	private String nationality;
-	private int playerScore = 0; //Default score that a player starts with
+	private int playerScore = 0;
 
 	// Constructors
 	public Player() {
-		setName("C.Ronaldo"); 
-		setNationality("Portugal");
-		setPlayerScore(0);
+		this.name = "C.Ronaldo"; 
+		this.nationality = "Portugal";
+		this.playerScore = 0;
 	}
 	public Player(String name, String nationality) {
-		setName(name);
-		setNationality(nationality);
-		setPlayerScore(0);
+		this.name = name;
+		this.nationality = nationality;
+		this.playerScore = 0;
 	}
 	// Getters and Setters
+        @Override
 	public String getName() {
 		return name;
 	}
+        @Override
 	public void setName(String name) {
 		this.name = name;
 	}
 
+        @Override
 	public String getNationality() {
 		return nationality;
 	}
+        @Override
 	public void setNationality(String nationality) {
 		this.nationality = nationality;
 	}
-
 	public int getPlayerScore() {
 		return playerScore;
 	}
 	public void setPlayerScore(int playerScore) {
-		if(playerScore>0){ //To make sure the players score is valid since the score cannot decrease 
-			this.playerScore += playerScore;
+		if(playerScore>0){
+		this.playerScore += playerScore;
 		}
 	}
 }
