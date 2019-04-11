a99c6ac0262992f70067bfb9688e16b22ed00e4d
MillionLinesOfCode <47336896+MillionLinesOfCode@users.noreply.github.com>
2019-04-11 17:32:01 -0600

    Added avatar file for abstraction

---
+++ Penalty Shootout Text Version/Avatar.java
@@ -0,0 +1,11 @@
+//This is an abstract class which has some defined some methods shared by the Player and Goalkeeper class.
+public abstract class Avatar {
+    
+        public abstract String getName();
+    
+        public abstract void setName(String name);
+
+	public abstract String getNationality();
+        
+	public abstract void setNationality(String nationality);
+}
